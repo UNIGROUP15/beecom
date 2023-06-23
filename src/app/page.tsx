d59a6b9a568d0c02f7dcbc75c81c2979da22883d
gothmate <gothmate@gmail.com>
2023-06-23 02:33:57 -0300

    Feature:  - criação do container do profile na página principal  - criada animação do "mostrar saldo" ao clicar no icone  - Separado o componente TopIndex da Página de Profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import Image from 'next/image'
 import styles from './page.module.sass'
-import SignIn from './signIn/page'
+// import SignIn from './signIn/page'
+import ProfileHome from './profilehome/page'
 import { Suspense } from 'react'
 import Loading from './loading'
 
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Suspense fallback={<Loading />}>
       <div className={styles.mainContainer}>
-        <SignIn />
+        <ProfileHome />
       </div>
     </Suspense>
 	)
