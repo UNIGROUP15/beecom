c8ea2e398632147dac109b84e700f0bcb242ecf2
gothmate <gothmate@gmail.com>
2023-06-24 19:19:53 -0300

    feature:  - animação página selecionada  - animação ícone selecionado  - adicionada condição de animação nas variáveis do sass

--- src/app/profilehome/page.tsx
+++ src/app/profilehome/page.tsx
@@ -6,7 +6,7 @@ export default function ProfileHome() {
   return (
 		<div className={styles.mainContainer}>
 			<TopIndex />
-			<FunctionalArea />
+			<FunctionalAreaHome />
 		</div>
 	)
 }
@@ -78,61 +78,101 @@ function TopIndex() {
 	);
 }
 
-function FunctionalArea() {
+function FunctionalAreaHome() {
 
 	function handleSearch() {
 		console.log('fiz a busca')
 	}
 
+	const [animation, setAnimation] = useState([
+		styles.functionBtnOff,
+		styles.functionBtnIn,
+		styles.functionBtnIn,
+		styles.functionBtnIn,
+		styles.functionBtnIn,
+	])
+
+	const [pageAnimate, setPageAnimate] = useState(styles.cards)
+
+	function animate(nome: string) {
+		nome == 'mercado' ? setAnimation([styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn])
+		: nome == 'ganhos' ? setAnimation([styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn])
+		: nome == 'carteira' ? setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn])
+		: nome == 'investimentos' ? setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn])
+		: nome == 'bitcoin' ? setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff])
+		: console.log('erro')
+
+		if(pageAnimate == styles.cards){
+			setPageAnimate(styles.cardsAnimate)
+		} else {
+			setPageAnimate(styles.cards)
+		}
+
+	}
+
 	return (
 		<div className={styles.areaContainer}>
-			<Image 
-				src={'/runner.svg'} 
-				alt='slide' 
-				width={24} 
-				height={24} 
-				priority 
-			/>
-	
-			<div className={styles.blockArea}>
-				<div className={styles.blurtop}></div>
+			<Image src={'/runner.svg'} alt='slide' width={24} height={24} priority />
 
+			<div className={styles.blockArea}>
 				<div className={styles.functions}>
-					<Image
-						src={'/market.svg'}
-						alt='market'
-						width={32}
-						height={32}
-						priority
-					/>
-					<Image
-						src={'/gains.svg'}
-						alt='gains'
-						width={32}
-						height={32}
-						priority
-					/>
-					<Image
-						src={'/wallet.svg'}
-						alt='wallet'
-						width={32}
-						height={32}
-						priority
-					/>
-					<Image
-						src={'/investments.svg'}
-						alt='investments'
-						width={32}
-						height={32}
-						priority
-					/>
-					<Image
-						src={'/bitcoin.svg'}
-						alt='bitcoin'
-						width={32}
-						height={32}
-						priority
-					/>
+					<div onClick={() => animate('mercado')} className={animation[0]}>
+						<Image
+							src={'/market.svg'}
+							alt='mercado'
+							width={32}
+							height={32}
+							priority
+						/>
+						<p>Mercado</p>
+					</div>
+
+					<div onClick={() => animate('ganhos')} className={animation[1]}>
+						<Image
+							src={'/gains.svg'}
+							alt='ganhos'
+							width={32}
+							height={32}
+							priority
+						/>
+						<p>Ganhos</p>
+					</div>
+
+					<div onClick={() => animate('carteira')} className={animation[2]}>
+						<Image
+							src={'/wallet.svg'}
+							alt='carteira'
+							width={32}
+							height={32}
+							priority
+						/>
+						<p>Carteira</p>
+					</div>
+
+					<div
+						onClick={() => animate('investimentos')}
+						className={animation[3]}
+					>
+						<Image
+							src={'/investments.svg'}
+							alt='investmentos'
+							width={32}
+							height={32}
+							priority
+						/>
+						<p>Investimentos</p>
+					</div>
+
+					<div onClick={() => animate('bitcoin')} className={animation[4]}>
+						<Image
+							src={'/bitcoin.svg'}
+							alt='bitcoin'
+							width={32}
+							height={32}
+							priority
+						/>
+						<p>Bitcoin</p>
+					</div>
 				</div>
 
 				<form className={styles.searchForm}>
@@ -145,82 +185,79 @@ function FunctionalArea() {
 						type='text'
 					/>
 				</form>
-				
-				<div className={styles.cardConteiner}>
-					<Image
-						src={'/Equinix.png'}
-						alt='Equinix'
-						width={40}
-						height={40}
-						priority
-					/>
-					<div>
-						<p className={styles.company}>EQIX34</p>
-						<p>13:42:28 | Equinix Inc Drm Ed</p>
+
+				<div className={pageAnimate}>
+
+					<div className={styles.cardConteiner}>
+						<Image
+							src={'/Equinix.png'}
+							alt='Equinix'
+							width={40}
+							height={40}
+							priority
+						/>
+						<div>
+							<p className={styles.company}>EQIX34</p>
+							<p>13:42:28 | Equinix Inc Drm Ed</p>
+						</div>
+						<div className={styles.color}>
+							<p className={styles.percent}>+2,56%</p>
+							<p className={styles.percent}>R$ 42,90</p>
+						</div>
 					</div>
-					<div className={styles.color}>
-						<p className={styles.percent}>+2,56%</p>
-						<p className={styles.percent}>R$ 42,90</p>
+
+					<div className={styles.cardConteiner}>
+						<Image
+							src={'/microsoft.png'}
+							alt='microsoft'
+							width={40}
+							height={40}
+							priority
+						/>
+						<div>
+							<p className={styles.company}>MSFT34</p>
+							<p>18:25:10 | Microsoft Drm Ed</p>
+						</div>
+						<div className={styles.color}>
+							<p className={styles.percent}>+3,29%</p>
+							<p className={styles.percent}>R$ 66,72</p>
+						</div>
+					</div>
+
+					<div className={styles.cardConteiner}>
+						<Image
+							src={'/netflix.png'}
+							alt='Netflix'
+							width={40}
+							height={40}
+							priority
+						/>
+						<div>
+							<p className={styles.company}>NFLX34</p>
+							<p>09:31:58 | Netflix Drm</p>
+						</div>
+						<div className={styles.color}>
+							<p className={styles.percentNeg}>-0,96%</p>
+							<p className={styles.percentNeg}>R$ 36,80</p>
+						</div>
+					</div>
+
+					<div className={styles.cardConteiner}>
+						<Image src={'/bmg.png'} alt='bmg' width={40} height={40} priority />
+						<div>
+							<p className={styles.company}>BMGB4</p>
+							<p>18:22:07 | Banco BMG Pn N1</p>
+						</div>
+						<div className={styles.color}>
+							<p className={styles.percent}>+0,45%</p>
+							<p className={styles.percent}>R$ 2,23</p>
+						</div>
 					</div>
 				</div>
 
-				<div className={styles.cardConteiner}>
-					<Image
-						src={'/microsoft.png'}
-						alt='microsoft'
-						width={40}
-						height={40}
-						priority
-					/>
-					<div>
-						<p className={styles.company}>MSFT34</p>
-						<p>18:25:10 | Microsoft Drm Ed</p>
-					</div>
-					<div className={styles.color}>
-						<p className={styles.percent}>+3,29%</p>
-						<p className={styles.percent}>R$ 66,72</p>
-					</div>
-				</div>
-
-				<div className={styles.cardConteiner}>
-					<Image
-						src={'/netflix.png'}
-						alt='Netflix'
-						width={40}
-						height={40}
-						priority
-					/>
-					<div>
-						<p className={styles.company}>NFLX34</p>
-						<p>09:31:58 | Netflix Drm</p>
-					</div>
-					<div className={styles.color}>
-						<p className={styles.percentNeg}>-0,96%</p>
-						<p className={styles.percentNeg}>R$ 36,80</p>
-					</div>
-				</div>
-
-				<div className={styles.cardConteiner}>
-					<Image
-						src={'/bmg.png'}
-						alt='bmg'
-						width={40}
-						height={40}
-						priority
-					/>
-					<div>
-						<p className={styles.company}>BMGB4</p>
-						<p>18:22:07 | Banco BMG Pn N1</p>
-					</div>
-					<div className={styles.color}>
-						<p className={styles.percent}>+0,45%</p>
-						<p className={styles.percent}>R$ 2,23</p>
-					</div>
-				</div>
-
+				<div className={styles.blurtop}></div>
 				<div className={styles.blur}></div>
 			</div>
-
 		</div>
 	);
 }
