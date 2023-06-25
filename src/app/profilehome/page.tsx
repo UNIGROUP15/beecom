fe7eccd1a3448adfcecf45d0e9e07e84b26fbb44
gothmate <gothmate@gmail.com>
2023-06-25 01:03:21 -0300

    update:  - feito map dos cards  - criado state com os objetos dos cards  - corrigido problema na animação dos itens  - criada tipagem dos cards  -

--- src/app/profilehome/page.tsx
+++ src/app/profilehome/page.tsx
@@ -15,7 +15,7 @@ function TopIndex() {
 
   const [isOpen, setIsOpen] = useState(true)
   const [olho, setOlho] = useState('/EyeOpen.svg')
-	const [amount, setAmount] = useState('3500,00')
+	const [amount, setAmount] = useState('13.500,00')
 
   function handleIsOpen() {
     if(isOpen === true) {
@@ -23,9 +23,9 @@ function TopIndex() {
       setOlho('/EyeClosed.svg')
 			setAmount('********')
     } else {
-			setIsOpen(true);
+			setIsOpen(true)
       setOlho('/EyeOpen.svg')
-			setAmount('3500,00');
+			setAmount('13.500,00')
     } 
 
   }
@@ -75,7 +75,24 @@ function TopIndex() {
 			</div>
 
 		</div>
-	);
+	)
+}
+
+interface Item {
+  img: string;
+  alt: string;
+  p1: string;
+  p2: string;
+  p3: string;
+  p4: string;
+}
+interface Card {
+  img: string;
+  alt: string;
+  p1: string;
+  p2: string;
+  p3: string;
+  p4: string;
 }
 
 function FunctionalAreaHome() {
@@ -92,15 +109,143 @@ function FunctionalAreaHome() {
 		styles.functionBtnIn,
 	])
 
+	const market: Item[] = [ 
+		{img: '/equinix.png', alt: 'Equinix', p1: 'EQIX34', p2: '13:42:28 | Equinix Inc Drm Ed', p3: '+2,56%', p4: 'R$ 42,90'}, 
+		{img: '/microsoft.png', alt: 'Microsoft', p1: 'MSFT34', p2: '10:31:17 | Microsoft Drm Ed', p3: '+3,29%', p4: 'R$ 66,71'}, 
+		{img: '/netflix.png', alt: 'Netflix', p1: 'NFLX34', p2: '16:50:01 | Netflix Drm', p3: '-0,96%', p4: 'R$ 36,00'}, 
+		{img: '/bmg.png', alt: 'Banco BMG', p1: 'BMGB4', p2: '17:41:58 | Banco BMG Pn N1', p3: '+0,45%', p4: 'R$ 2,23'}
+	]
+
+	const gains: Item[] = [
+		{
+			img: '/btc.png',
+			alt: 'BTC',
+			p1: 'Total Investido:',
+			p2: 'R$ 800,00',
+			p3: 'Total Atual',
+			p4: '10.518,45',
+		},
+		{
+			img: '/eth.png',
+			alt: 'Etherial',
+			p1: 'Total Investido:',
+			p2: 'R$300,00',
+			p3: 'Total Atual',
+			p4: 'R$ 818,45',
+		},
+	];
+	const wallet: Item[] = [
+		{
+			img: '/btc.png',
+			alt: 'BTC',
+			p1: 'BTC/BRL',
+			p2: '12/08/20021 | R$ 800,00',
+			p3: 'R$ 10.518,45',
+			p4: '+0,33%',
+		},
+		{
+			img: '/eth.png',
+			alt: 'Etherial',
+			p1: 'ETH/BRL',
+			p2: '12/08/20021 | R$ 300,00',
+			p3: 'R$ 818,45',
+			p4: '+103,22%',
+		},
+	];
+	const investments: Item[] = [
+		{
+			img: '/equinix.png',
+			alt: 'Equinix',
+			p1: 'EQIX34',
+			p2: '13:42:28 | Equinix Inc Drm Ed',
+			p3: '+2,56%',
+			p4: 'R$ 42,90',
+		},
+		{
+			img: '/microsoft.png',
+			alt: 'Microsoft',
+			p1: 'EQIX34',
+			p2: '13:42:28 | Equinix Inc Drm Ed',
+			p3: '+2,56%',
+			p4: 'R$ 42,90',
+		},
+		{
+			img: '/netflix.png',
+			alt: 'Netflix',
+			p1: 'EQIX34',
+			p2: '13:42:28 | Equinix Inc Drm Ed',
+			p3: '+2,56%',
+			p4: 'R$ 42,90',
+		},
+		{
+			img: '/bmg.png',
+			alt: 'Banco BMG',
+			p1: 'EQIX34',
+			p2: '13:42:28 | Equinix Inc Drm Ed',
+			p3: '+2,56%',
+			p4: 'R$ 42,90',
+		},
+	];
+
+	const cripto: Item[] = [
+		{
+			img: '/btc.png',
+			alt: 'BTC',
+			p1: 'BTC/BRL',
+			p2: '10:30:38 | Bitcoin',
+			p3: '+0,68%',
+			p4: 'R$ 131.518,45',
+		},
+		{
+			img: '/eth.png',
+			alt: 'Etherial',
+			p1: 'ETH/BRL',
+			p2: '15:22:08 | Ether',
+			p3: '+0,76%',
+			p4: 'R$ 8.997,79',
+		},
+		{
+			img: '/doge.png',
+			alt: 'Doge',
+			p1: 'DOGE/BRM',
+			p2: '19:02:28 | Dogecoin',
+			p3: '-0,33%',
+			p4: 'R$ 0,3515',
+		},
+		{
+			img: '/ltc.png',
+			alt: 'LTC',
+			p1: 'LTC/BRL',
+			p2: '08:20:00 | Litecoin',
+			p3: '+0,42%',
+			p4: 'R$ 423,58',
+		},
+	];
+
 	const [pageAnimate, setPageAnimate] = useState(styles.cards)
+	const [cards, setCards] = useState(market)
 
 	function animate(nome: string) {
-		nome == 'mercado' ? setAnimation([styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn])
-		: nome == 'ganhos' ? setAnimation([styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn])
-		: nome == 'carteira' ? setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn])
-		: nome == 'investimentos' ? setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn])
-		: nome == 'bitcoin' ? setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff])
-		: console.log('erro')
+		if(nome == 'mercado') {
+			setAnimation([styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn])
+				setCards(market)
+		}
+		if(nome == 'ganhos') {
+			setAnimation([styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn])
+			setCards(gains)
+		}
+		if(nome == 'carteira') {
+			setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn, styles.functionBtnIn])
+			setCards(wallet)
+		}
+		if(nome == 'investimentos') {
+			setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff, styles.functionBtnIn])
+			setCards(investments)
+		}
+		if(nome == 'bitcoin') {
+			setAnimation([styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnIn, styles.functionBtnOff])
+			setCards(cripto)
+		}
 
 		if(pageAnimate == styles.cards){
 			setPageAnimate(styles.cardsAnimate)
@@ -187,72 +332,29 @@ function FunctionalAreaHome() {
 				</form>
 
 				<div className={pageAnimate}>
-		
-					<div className={styles.cardConteiner}>
-						<Image
-							src={'/Equinix.png'}
-							alt='Equinix'
-							width={40}
-							height={40}
-							priority
-						/>
-						<div>
-							<p className={styles.company}>EQIX34</p>
-							<p>13:42:28 | Equinix Inc Drm Ed</p>
-						</div>
-						<div className={styles.color}>
-							<p className={styles.percent}>+2,56%</p>
-							<p className={styles.percent}>R$ 42,90</p>
-						</div>
-					</div>
 
-					<div className={styles.cardConteiner}>
-						<Image
-							src={'/microsoft.png'}
-							alt='microsoft'
-							width={40}
-							height={40}
-							priority
-						/>
-						<div>
-							<p className={styles.company}>MSFT34</p>
-							<p>18:25:10 | Microsoft Drm Ed</p>
-						</div>
-						<div className={styles.color}>
-							<p className={styles.percent}>+3,29%</p>
-							<p className={styles.percent}>R$ 66,72</p>
-						</div>
-					</div>
+					{cards.map((card: Card, index: number) => {
+						return (
+							<div className={styles.cardConteiner} key={index}>
+								<Image
+									src={card.img}
+									alt={card.alt}
+									width={40}
+									height={40}
+									priority
+								/>
+								<div>
+									<p className={styles.company}>{card.p1}</p>
+									<p>{card.p2}</p>
+								</div>
+								<div className={styles.color}>
+									<p className={styles.percent}>{card.p3}</p>
+									<p className={styles.percent}>{card.p4}</p>
+								</div>
+							</div>
+						)
+					})}
 
-					<div className={styles.cardConteiner}>
-						<Image
-							src={'/netflix.png'}
-							alt='Netflix'
-							width={40}
-							height={40}
-							priority
-						/>
-						<div>
-							<p className={styles.company}>NFLX34</p>
-							<p>09:31:58 | Netflix Drm</p>
-						</div>
-						<div className={styles.color}>
-							<p className={styles.percentNeg}>-0,96%</p>
-							<p className={styles.percentNeg}>R$ 36,80</p>
-						</div>
-					</div>
-
-					<div className={styles.cardConteiner}>
-						<Image src={'/bmg.png'} alt='bmg' width={40} height={40} priority />
-						<div>
-							<p className={styles.company}>BMGB4</p>
-							<p>18:22:07 | Banco BMG Pn N1</p>
-						</div>
-						<div className={styles.color}>
-							<p className={styles.percent}>+0,45%</p>
-							<p className={styles.percent}>R$ 2,23</p>
-						</div>
-					</div>
 				</div>
 
 				<div className={styles.blurtop}></div>
